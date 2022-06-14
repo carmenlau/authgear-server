46daa11014c1b49df30cd34e767ba31baa6d9f4d
Carmen <carmen@oursky.com>
2022-06-14 15:41:20 +0100

    Collect active user count

--- pkg/lib/usage/usage_record.go
+++ pkg/lib/usage/usage_record.go
@@ -1,5 +1,12 @@
 package usage
 
+import (
+	"time"
+
+	"github.com/authgear/authgear-server/pkg/util/periodical"
+	"github.com/authgear/authgear-server/pkg/util/uuid"
+)
+
 type UsageRecordName string
 
 const (
@@ -8,3 +15,27 @@ const (
 	EmailSent           UsageRecordName = "email-sent"
 	WhatsappOTPVerified UsageRecordName = "whatsapp-otp-verified"
 )
+
+type UsageRecord struct {
+	ID             string
+	AppID          string
+	Name           UsageRecordName
+	Period         string
+	StartTime      time.Time
+	EndTime        time.Time
+	Count          int
+	AlertData      map[string]interface{}
+	StripTimestamp *time.Time
+}
+
+func NewUsageRecord(appID string, name UsageRecordName, count int, period periodical.Type, startTime time.Time, endTime time.Time) *UsageRecord {
+	return &UsageRecord{
+		ID:        uuid.New(),
+		AppID:     appID,
+		Name:      name,
+		Count:     count,
+		Period:    string(period),
+		StartTime: startTime,
+		EndTime:   endTime,
+	}
+}
